fc210dd74ad1e7e70555b7352b91f1c9b718f43c
Cogo <86952093+CogoCoding@users.noreply.github.com>
2022-01-20 18:56:40 +0900

    Update SearchInsertPosition_035.java

--- LeetCode/StudyPlan_Algorithm/SearchInsertPosition_035.java
+++ LeetCode/StudyPlan_Algorithm/SearchInsertPosition_035.java
@@ -1,7 +1,28 @@
 package LeetCode.StudyPlan_Algorithm;
 
-// #문제
-//
+// 타인 모범 풀이
+// The crux over here is that the array is already sorted.
+// We are comparing the first and last elements because after square these have the possibility of being the highest element.
+// Both the extremes contain the max element (after square ofc), so we are inserting these elements to the last of the new array to make it sorted.
+
+//   public int[] sortedSquares(int[] A) {
+//        int[] res = new int[A.length];
+//        int start = 0, end = A.length-1;
+//        int resIndex = A.length-1;
+       
+//        while(start <= end){
+//            if(A[start]*A[start] > A[end]*A[end]){
+//                res[resIndex--] = A[start]*A[start];
+//                start++;
+//            }else{
+//                res[resIndex--] = A[end]*A[end];
+//                end--;
+//            }
+//        }
+       
+//        return res;
+//    }
+
 public class SearchInsertPosition_035 {
     public static void main(String[] args) {
 //        int[] nums = {1,3,5,6}; //2->1 , 4->3, 7->4
