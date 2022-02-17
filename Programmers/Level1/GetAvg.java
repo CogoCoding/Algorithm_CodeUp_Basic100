6337b9abca14e3086634ca41870f13d94e148f43
sophia <sheisinthezone@gmail.com>
2022-02-17 19:03:13 +0900

    GetAvg.java

---
+++ Programmers/Level1/GetAvg.java
@@ -0,0 +1,13 @@
+package Programmers.Level1;
+
+public class GetAvg {
+    class Solution {
+        public double solution(int[] arr) {
+            double answer = 0;
+            for(int i=0;i<arr.length;i++){
+                answer+=arr[i];
+            }
+            return answer/arr.length;
+        }
+    }
+}
